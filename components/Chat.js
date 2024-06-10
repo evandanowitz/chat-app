02dd6ed805275f683b2a945591e36609c586d8a4
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-06-10 18:27:33 -0400

    Create 'messages' state variable

--- components/Chat.js
+++ components/Chat.js
@@ -6,8 +6,9 @@ const ChatScreen = ({ route, navigation }) => {
   // route.params is a React Navigation object. Contains paramaters passed to the ChatScreen when it was navigated to from StartScreen
     // used to unpack properties from objects into variables
   const { name, bgColor } = route.params;
+  // messages state makes sense here, as a chat app needs to send, receive, and display messages.
+  const [messages, setMessages] = useState([]);
 
-  // functions in useEffect are executed after the component is rendered/mounted
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
