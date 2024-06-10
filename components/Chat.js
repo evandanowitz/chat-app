7ee98594fd127c6db18213c9348bca8c8256133c
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-06-10 18:29:06 -0400

    Update comment type to multi-line

--- components/Chat.js
+++ components/Chat.js
@@ -3,8 +3,8 @@ import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { Bubble, GiftedChat } from 'react-native-gifted-chat'; // import GiftedChat library for chat interface
 
 const ChatScreen = ({ route, navigation }) => {
-  // route.params is a React Navigation object. Contains paramaters passed to the ChatScreen when it was navigated to from StartScreen
-    // used to unpack properties from objects into variables
+  /* route.params is a React Navigation object. Contains paramaters passed to the ChatScreen when 
+  it was navigated to from StartScreen used to unpack properties from objects into variables */
   const { name, bgColor } = route.params;
   // messages state makes sense here, as a chat app needs to send, receive, and display messages.
   const [messages, setMessages] = useState([]);
