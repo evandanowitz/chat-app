fd71311aa9677e2533da8242e848e6ce42f42c49
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-06-11 13:38:13 -0400

    Update comments for easier readability

--- components/Start.js
+++ components/Start.js
@@ -4,74 +4,50 @@ import { StyleSheet, View, Text, TextInput, ImageBackground, TouchableOpacity } 
 const StartScreen = ({ navigation }) => {
   const [name, setName] = useState('');
   const imageBackground = require('../assets/background-img.png');
-  // Add icon image from assets folder to TextInput using Image component from react-native
   const [bgColor, setBgColor] = useState('');
+  // Add icon image from assets folder to TextInput using Image component from react-native
 
   return (
-    // Entire component container
-    <View style={styles.container}>
-      {/* App image background */}
-      <ImageBackground style={styles.imageBackground} source={imageBackground}>
-        {/* App Title */}
-        <Text style={styles.title}>Chat App</Text>
-        {/* View component holding all main elements */}
-        <View style={styles.box}>
-          {/* TextInput */}
-          <TextInput
-            style={styles.textInput}
+    <View style={styles.container}> {/* Entire component container */}
+      <ImageBackground style={styles.imageBackground} source={imageBackground}> {/* App image background */}
+        <Text style={styles.title}>Chat App</Text> {/* App Title */}
+        <View style={styles.box}> {/* View component holding all main elements */}
+          <TextInput style={styles.textInput} // TextInput
             value={name}
             onChangeText={setName}
-            placeholder="Your Name"
+            placeholder='Your Name' 
           />
-          {/* Entire background color element */}
-          <View style={styles.bgColorBox}>
-            {/* Choose Background Color text */}
-            <Text style={styles.chooseBgColorText}>Choose Background Color:</Text>
-            {/* Container holding all background color buttons */}
-            <View style={styles.bgColorButtonsContainer}>
-              {/* Individual background color button */}
-              <TouchableOpacity 
-                style={[
-                  styles.bgColorButtons, 
+          <View style={styles.bgColorBox}> {/* Entire background color element */}
+            <Text style={styles.chooseBgColorText}>Choose Background Color:</Text> {/* Choose Background Color text */}
+            <View style={styles.bgColorButtonsContainer}> {/* Container holding all background color buttons */}
+              <TouchableOpacity // Individual background color button
+                style={[ styles.bgColorButtons, 
                   { backgroundColor: '#090C08' }, 
                   bgColor === '#090C08' && styles.selectedBgColor
-                ]}
-                onPress={() => setBgColor('#090C08')}
-              />
-              {/* Individual background color button */}
-              <TouchableOpacity
-                style={[
-                  styles.bgColorButtons, 
+                ]} 
+                onPress={() => setBgColor('#090C08')} />
+              <TouchableOpacity // Individual background color button
+                style={[ styles.bgColorButtons, 
                   { backgroundColor: '#474056' }, 
                   bgColor === '#474056' && styles.selectedBgColor
                 ]}  
-                onPress={() => setBgColor('#474056')}
-              />
-              {/* Individual background color button */}
-              <TouchableOpacity
-                style={[
-                  styles.bgColorButtons, 
+                onPress={() => setBgColor('#474056')} />
+              <TouchableOpacity // Individual background color button
+                style={[ styles.bgColorButtons, 
                   { backgroundColor: '#8A95A5' }, 
                   bgColor === '#8A95A5' && styles.selectedBgColor
                 ]}  
-                onPress={() => setBgColor('#8A95A5')}
-              />
-              {/* Individual background color button */}
-              <TouchableOpacity
-                style={[
-                  styles.bgColorButtons, 
-                  { backgroundColor: '#B9C6AE' }, 
-                  bgColor === '#B9C6AE' && styles.selectedBgColor
-                ]}  
-                onPress={() => setBgColor('#B9C6AE')}
-              />
+                onPress={() => setBgColor('#8A95A5')} />
+              <TouchableOpacity // Individual background color button
+                style={[ styles.bgColorButtons, 
+                { backgroundColor: '#B9C6AE' }, 
+                bgColor === '#B9C6AE' && styles.selectedBgColor
+                ]} 
+                onPress={() => setBgColor('#B9C6AE')} />
             </View>
           </View>
-          
-          {/* Start Chat button. TouchableOpacity is a more customizable button. Added bgColor to navigate */}
-          <TouchableOpacity 
-            style={styles.chatButton}
-            title="Start Chatting"
+          <TouchableOpacity style={styles.chatButton} // Chat button. TouchableOpacity more customizable. Added bgColor to navigate.
+            title='Start Chatting'
             onPress={() => navigation.navigate('ChatScreen', { name: name, bgColor: bgColor })}
           >
             <Text style={styles.chatButtonText}>Start Chatting</Text>
@@ -84,14 +60,12 @@ const StartScreen = ({ navigation }) => {
 
 // STILL NEED TO CUT DOWN ON DUPLICATE CODE LINES IN STYLES. See Exercise 5.1
 const styles = StyleSheet.create({
-  // Entire app container for main View component
-  container: {
+  container: { // Entire app container for main View component
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
   },
-  // Text input where users put in their name to be displayed in chat screen
-  textInput: {
+  textInput: { // Text input where users put in their name to be displayed in chat screen
     fontSize: 16,
     fontWeight: '300',
     color: '#757083',
@@ -101,8 +75,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginTop: 20,
   },
-  // Actual chat button
-  chatButton: {
+  chatButton: { // Actual chat button
     width: '88%',
     backgroundColor: '#757083',
     padding: 15,
@@ -110,14 +83,12 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     alignItems: 'center'
   },
-  // Text displaying on chat button
-  chatButtonText: {
+  chatButtonText: { // Text displaying on chat button
     fontSize: 16,
     fontWeight: '600',
     color: '#fff'
   },
-  // App title
-  title: {
+  title: { // App title
     position: 'absolute',
     top: 0,
     color: '#fff',
@@ -125,52 +96,45 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     marginTop: 100
   },
-  // Holds everything related to background color stuff
-  bgColorBox: {
+  bgColorBox: { // Holds everything related to background color stuff
     alignSelf: 'flex-start',
     marginLeft: 20,
   },
-  // Text that tells users to choose their background color of choice
-  chooseBgColorText: {
+  chooseBgColorText: { // Text that tells users to choose their background color of choice
     fontSize: 16,
     fontWeight: '300',
     color: '#757083',
     opacity: 1,
     marginBottom: 20
   },
-  // Container holding all background color buttons
-  bgColorButtonsContainer: {
+  bgColorButtonsContainer: { // Container holding all background color buttons
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
-  // Individual background color buttons
-  bgColorButtons: {
+  bgColorButtons: { // Individual background color buttons
     width: 40,
     height: 40,
     borderRadius: 20
   },
-  // Background color that was selected by user
-  selectedBgColor: {
+  selectedBgColor: { // Background color that was selected by user
     borderWidth: 3,
     borderColor: '#000'
   },
-  // Background image for app's main screen
-  imageBackground: {
+  imageBackground: { // Background image for app's main screen
     flex: 1,
     width: '100%',
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center'
   },
-  // View component holding TextInput, background color stuff, and chat button
-  box: {
+  box: { // View component holding TextInput, background color stuff, and chat button
     position: 'absolute',
     bottom: 25,
     backgroundColor: '#fff',
     width: '88%',
     height: '44%',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   }
 });
 
