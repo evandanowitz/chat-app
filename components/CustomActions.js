d797c4538f727d6b0f3a14499284b87acb7b23d6
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-06-24 18:34:38 -0400

    Import 'ImagePicker' and 'Location' Expo packages

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,3 +1,5 @@
+import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
 const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID }) => {
 }
 
