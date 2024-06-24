f39c94589d0e57c1c427391fa18222804e73b18f
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-06-24 18:37:48 -0400

    Add styles for the CustomActions button

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -5,3 +5,26 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID })
 }
 
 export default CustomActions;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: '#b2b2b2',
+    borderWidth: 2,
+    flex: 1
+  },
+iconText: {
+  color: '#b2b2b2',
+  fontWeight: 'bold',
+  fontSize: 20,
+  backgroundColor: 'transparent',
+  textAlign: 'center',
+  marginTop: -2.5
+}
+});
