cf491ee0e2a401d93c372421165a44bd736fc782
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-06-24 18:36:14 -0400

    Import react-native core components

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,3 +1,4 @@
+import { TouchableOpacity, Text, View, StyleSheet, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
 const CustomActions = ({ wrapperStyle, iconTextStyle, onSend, storage, userID }) => {
